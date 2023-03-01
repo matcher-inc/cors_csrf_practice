8bac5afe13c9f0646662cb67567dfa18d1f90da0
kouphasi <phasi.ok.phasi@gmail.com>
2023-03-01 18:28:00 +0900

    失敗

--- rails/backend/app/controllers/test_controller.rb
+++ rails/backend/app/controllers/test_controller.rb
@@ -1,9 +1,16 @@
 class TestController < ApplicationController
+  after_action :set_csrf_token_header, only: [:csrf_token_api]
+  skip_before_action :verify_authenticity_token
   def get_api
     render json: :ok
   end
 
   def post_api
+    puts request
+    render json: :ok
+  end
+
+  def csrf_token_api 
     render json: :ok
   end
 end
